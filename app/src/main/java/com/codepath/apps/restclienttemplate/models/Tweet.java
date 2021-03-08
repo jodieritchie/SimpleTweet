51c070c3eccfec32fdf4b3f33ac209e181f54380
Jodie Ritchie <jfritchie@wisc.edu>
2021-03-07 22:17:34 -0600

    Updated client

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -25,7 +25,7 @@ public class Tweet {
 
     public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
         List<Tweet> tweets = new ArrayList<>();
-        for(int i = 0; i < jsonArray.length(); ++i){
+        for(int i = 0; i < jsonArray.length(); i++){
             tweets.add(fromJson(jsonArray.getJSONObject(i)));
         }
         return tweets;
